45dcd1007049b984d5a4f47a7c7810c32aac9b87
bigb <brendan.moy@gmail.com>
2021-01-02 14:51:46 -0500

    refactor and add colored buttons

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
   def new
     @article = Article.new
   end
@@ -10,7 +12,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-     flash[:notice] = "Article was successfully created"
+     flash[:success] = "Article was successfully created"
      redirect_to article_path(@article)
     else
      render 'new'
@@ -18,27 +20,34 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
-     flash[:notice] = "Article was updated"
+     flash[:success] = "Article was updated"
      redirect_to article_path(@article)
     else
-     flash[:notice] = "Article was not updated"
+     flash[:success] = "Article was not updated"
      render 'edit'
     end
+  end
+
+  def destroy
+    @article.destroy
+    flash[:success] = "Article was deleted"
+    redirect_to articles_path
   end
 
   private
   
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
