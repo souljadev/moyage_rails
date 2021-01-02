88f5f714c2eb09a4a9a5a77fbf0c5d650b6c5d14
bigb <brendan.moy@gmail.com>
2021-01-02 16:21:41 -0500

    create users table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_13_154939) do
+ActiveRecord::Schema.define(version: 2021_01_02_200219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2020_12_13_154939) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
