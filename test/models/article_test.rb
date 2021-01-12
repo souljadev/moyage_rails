05b6ed0430131f82d7280bfee17112e2b1695aa7
bigb <brendan.moy@gmail.com>
2021-01-11 22:59:46 -0500

    add test for article model

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,8 +1,33 @@
 require "test_helper"
+require "pry"
 
 class ArticleTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
-    p 'test here'
+  test "should not save article without valid user" do
+    article = Article.new(
+      title: "Hello I am a test", 
+      description: "Test Description"
+    )
+    article.save
+    assert article.errors.full_messages, "User must exist"
   end
+
+  test "should not save article without valid user" do
+    article = Article.new(
+      title: "Hel", 
+      description: "Test Description"
+    )
+    article.save
+    assert article.errors.full_messages, "User must exist"
+  end  
+
+  test "should not save article without title" do
+    article = Article.new(description: "Test Description")
+    assert_not article.save, "Article was successfully created"
+  end
+
+  test "should not save article without description" do
+    article = Article.new(title: "Test Title")
+    assert_not article.save, "Saved the article without a title"
+  end  
+
 end
