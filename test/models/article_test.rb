fa1c97ee3c839da16bdade535e04c31cc4d9f73b
bigb <brendan.moy@gmail.com>
2021-02-17 00:38:47 -0500

    edit show articles

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -2,18 +2,24 @@ require "test_helper"
 require "pry"
 
 class ArticleTest < ActiveSupport::TestCase
-  test "should not save article without valid user" do
+
+  test "article saved successfully" do
+    user = User.new(username: "test1", email: "test1@gmail.com", password_digest: "123")
+    
+    user.save
+
     article = Article.new(
       title: "Hello I am a test", 
-      description: "Test Description"
+      description: "Test Description",
+      user_id: user.id
     )
-    article.save
-    assert article.errors.full_messages, "User must exist"
+    assert(article.save)
+    user.delete
   end
 
   test "should not save article without valid user" do
     article = Article.new(
-      title: "Hel", 
+      title: "Hello", 
       description: "Test Description"
     )
     article.save
